fa362c63197fa0a94b30225ef735553e43d7e122
Quang Vu Ngoc <vungocquang27102001@gmail.com>
2024-06-04 10:00:11 +0700

    create project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading1, setIsLoading1] = useState<any>('123');
+  const [isLoading2, setIsLoading2] = useState<boolean>(true);
+
+  useEffect(() => {
+    // trong này là 1 cái hàm gì đó....
+  }, []);
+
+  const ham = () => {
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code></code> and save to reload.
         </p>
         <a
           className="App-link"
